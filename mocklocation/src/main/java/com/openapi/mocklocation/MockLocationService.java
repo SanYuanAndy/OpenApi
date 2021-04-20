eb9e77e1d7b381eb07aaaa12c1cd132b5a61e740
SanYuanAndy <andyzsy@outlook.com>
2021-04-20 14:10:55 +0800

    add new case

--- mocklocation/src/main/java/com/openapi/mocklocation/MockLocationService.java
+++ mocklocation/src/main/java/com/openapi/mocklocation/MockLocationService.java
@@ -1,8 +1,14 @@
 package com.openapi.mocklocation;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+
+import androidx.annotation.RequiresApi;
 
 public class MockLocationService extends Service {
     public MockLocationService() {
@@ -21,7 +27,22 @@ public class MockLocationService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        keepAlive();
         VirtualLocationManager.getInstance().onReceive(this, (Intent)intent.getParcelableExtra("intent"));
         return super.onStartCommand(intent, flags, startId);
     }
+
+    private void  keepAlive() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "mock_location";
+            NotificationChannel channel = new NotificationChannel(channelId, "mock_location", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+            Notification notification = new Notification.Builder(this, channelId).build();
+            startForeground(1, notification);
+        }
+    }
+
 }
--- mocklocation/src/main/java/com/openapi/mocklocation/MockReceiver.java
+++ mocklocation/src/main/java/com/openapi/mocklocation/MockReceiver.java
@@ -11,7 +11,7 @@ public class MockReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.e("MockReceiver", "onReceive");
-        Intent i =new Intent(context, MockLocationService.class);
+        Intent i = new Intent(context, MockLocationService.class);
         i.putExtra("intent", intent);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             context.startForegroundService(i);
