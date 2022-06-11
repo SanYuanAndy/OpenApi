260496d9242bfa781639c2e76c1f957f5a0366fb
SanYuanAndy <andyzsy@outlook.com>
2022-06-11 13:23:36 +0800

    新增功能

--- app/src/main/java/com/example/myapplication/DemoCaseActivity.java
+++ app/src/main/java/com/example/myapplication/DemoCaseActivity.java
@@ -2,8 +2,20 @@ package com.example.myapplication;
 
 import android.widget.Toast;
 
+import com.openapi.comm.utils.DnsParser;
+import com.openapi.comm.utils.HttpManager;
+import com.openapi.comm.utils.LogUtil;
+import com.openapi.comm.utils.WorkHandler;
 import com.openapi.debugger.ActionAdapter;
 import com.openapi.debugger.DebuggerActivity;
+import com.openapi.debugger.MonkeyService;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DemoCaseActivity extends DebuggerActivity {
 
@@ -32,9 +44,102 @@ public class DemoCaseActivity extends DebuggerActivity {
                 return false;
             }
         });
+
+        addAction(new ActionAdapter.Action("Http") {
+            @Override
+            public boolean invoke() {
+                HttpManager.getInstance().request("http://customer.cx580.com/", new HttpManager.RequestCallBack() {
+                    @Override
+                    public void onError(int code, String errMsg) {
+                        LogUtil.e("checkWhitelist onError", "code:" + code + ", errMsg:" + errMsg);
+                    }
+
+                    @Override
+                    public void onSuccess(int code, String bodyString) {
+                        LogUtil.e("checkWhitelist onSuccess", "code:" + code + ", body:" + bodyString);
+                    }
+
+                });
+                return false;
+            }
+        });
+
+        addAction(new ActionAdapter.Action("monkey") {
+            @Override
+            public boolean invoke() {
+                WorkHandler.runBgThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        MonkeyService.run();
+                    }
+                }, 0);
+                return false;
+            }
+        });
+
+        addAction(new ActionAdapter.Action("getDnsAndConnect") {
+            @Override
+            public boolean invoke() {
+                WorkHandler.runBgThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        checkWhitelist();
+                    }
+                }, 0);
+                return false;
+            }
+        });
+
+        addAction(new ActionAdapter.Action("parseDns") {
+            @Override
+            public boolean invoke() {
+                WorkHandler.runBgThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<String> dnsList = new ArrayList<>();
+                        dnsList.add("download.ali.xmcdn.com");
+                        dnsList.add("oss.cx580.com");
+                        dnsList.add("oss.cx580.com.w.kunlunaq.com");
+                        dnsList.add("customer.cx580.com");
+                        dnsList.add("violation-bapi.cx580.com");
+                        dnsList.add("nlchshjapi.cx580.com");
+                        Map<String, Set<String>> map = DnsParser.getInstance().parseAllDns(dnsList, 10, 5000);
+                        for (String key : map.keySet()) {
+                            for (String ip : map.get(key)) {
+                                LogUtil.e("checkWhitelist", key + "," + ip);
+
+                            }
+                        }
+                    }
+                }, 0);
+                return false;
+            }
+        });
+
     }
 
     public static String getDebugLabel() {
         return "DemoCaseActivity";
     }
+
+    private static void checkWhitelist() {
+        try {
+            InetAddress[] remotes = InetAddress.getAllByName("download.ali.xmcdn.com");
+            for (InetAddress remote : remotes) {
+                LogUtil.e("checkWhitelist", "address:" + remote);
+                try {
+                    Socket socket = new Socket(remote.getHostAddress(), 80);
+                    LogUtil.e("checkWhitelist", "connect");
+                } catch (Exception e) {
+                    LogUtil.e("checkWhitelist", e.toString() + ":" + remote.getHostAddress());
+                }
+            }
+        } catch (Exception e) {
+            LogUtil.e("checkWhitelist", e.toString());
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
