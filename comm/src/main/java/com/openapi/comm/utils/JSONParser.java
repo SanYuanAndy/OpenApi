ac690235b8a0515c26e526ffccd4ec357b725023
SanYuanAndy <andyzsy@outlook.com>
2022-06-24 13:23:28 +0800

    新增功能

--- comm/src/main/java/com/openapi/comm/utils/JSONParser.java
+++ comm/src/main/java/com/openapi/comm/utils/JSONParser.java
@@ -43,4 +43,82 @@ public class JSONParser {
 
         return obj;
     }
+
+    public static <T> T parseDeep(Class<T> clazz, JSONObject jsonObj) {
+        T obj = null;
+        try {
+            obj = clazz.newInstance();
+        } catch (Exception e) {
+            LogUtil.e(TAG, "" + e);
+        }
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                Object val = deep(field, jsonObj.get(field.getName()));
+                field.setAccessible(true);
+                field.set(obj, val);
+            } catch (Exception e) {
+                LogUtil.e(TAG, "" + e);
+            }
+        }
+
+        return obj;
+    }
+
+    public static <T> List<T> parseDeep(Class<T> clazz, JSONArray jsonArray) {
+        List<T> objList = new ArrayList<>();
+
+        for(int i = 0; i < jsonArray.length(); ++i) {
+            try {
+                Object value = jsonArray.get(i);
+                T obj = null;
+                if (value instanceof JSONObject) {
+                    obj = parseDeep(clazz, (JSONObject) value);
+                } else if (value instanceof JSONArray) {
+                    // todo
+                } else {
+                    obj = (T) value;
+                }
+                objList.add(obj);
+            } catch (Exception e) {
+
+            }
+        }
+        return objList;
+    }
+
+    public static <T> T parseDeep(Class<T> clazz, String strJson) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(strJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsonObject = new JSONObject();
+        }
+
+        return parseDeep(clazz, jsonObject);
+    }
+
+    public static <T> List<T> parseDeepArray(Class<T> clazz, String strJson) {
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = new JSONArray(strJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsonArray = new JSONArray();
+        }
+
+        return parseDeep(clazz, jsonArray);
+    }
+
+    private static Object deep(Field field, Object val) {
+        if (val instanceof JSONObject) {
+            return parseDeep(field.getType(), (JSONObject) val);
+        } else if (val instanceof JSONArray) {
+            return parseDeep(field.getType().getComponentType(),(JSONArray) val);
+        } else {
+            return val;
+        }
+    }
 }
